1dee9d9ceb459da8b06356fc0eb6d2cba6a95db8
Matthew Wells <matthewpwells@gmail.com>
2015-02-08 09:55:00 -0800

    Refactor some code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,18 @@
-//Global variable for the enemy start x coordinate
-var enemyXStart = -140;
-
-//Global variable that toggles on/off if the player wins
-var gameWon = false;
-
-//Global variable for the level of the game
-var gameLevel = 1;
-
-//Global variable for number of lives the player has
-var gameLives = 5;
-
-//Global variables for the player start coordinates
-var playerXStart = 200;
-var playerYStart = 400;
-
-//Global variables for enemy area border y coordinates
-var enemyAreaTop = 25;
-var enemyAreaBottom = 250;
+var gameData = {
+    "gameLevel":1,
+    "gameLives":5,
+    "gameWon": false,
+    "rows": {
+        "row1": 50,
+        "row2": 140,
+        "row3": 220
+    }
+};
 
 // Enemies our player must avoid
 var Enemy = function(x,y) {
+    //Variable for the enemy start x coordinate
+    var enemyXStart = -140;
     //set the x and y coordinates equal to the supplied numbers
     this.x = x;
     this.y = y;
@@ -45,9 +38,9 @@ Enemy.prototype.update = function(dt) {
 
     //check to see if gameWon is true or gameLevel is below
     //zero. If so, respawn more enemies by a factor of the new level
-    if (gameWon === true || gameLevel < 0) {
+    if (gameData.gameWon === true || gameData.gameLevel < 0) {
         allEnemies = [];
-        Enemies.spawn(gameLevel * 5);
+        Enemies.spawn(gameData.gameLevel * 5);
     };
     //update the canvas to put the enemy further across
     //the screen by the rate * dt * this.speed factor;
@@ -68,6 +61,10 @@ Enemy.prototype.render = function() {
 
 //The function for our hero 
 var player = function() {
+    //Player start coordinates
+    var playerXStart = 200;
+    var playerYStart = 400;
+    
     //Set the starting x and y coordinates
     this.x = playerXStart;
     this.y = playerYStart;
@@ -95,12 +92,12 @@ player.prototype.update = function() {
     if (this.y <= winner) {
         this.x = playerXStart;
         this.y = playerYStart;
-        gameWon = true;
-        gameLevel ++;
+        gameData.gameWon = true;
+        gameData.gameLevel ++;
     //If the game is not a winner, check if the player has fewer 
     //than 0 lives. If so, set the player to start, set level to 1 
-    } else if (gameLives < 0) {
-        gameLevel = 1;
+    } else if (gameData.gameLives < 0) {
+        gameData.gameLevel = 1;
         this.x = playerYStart;
         this.y = playerYStart;
     //If the player is not at the winning y coordinate, check
@@ -113,7 +110,7 @@ player.prototype.update = function() {
     } else if (this.y > bottomBorder) {
         this.y = bottomBorder;
     } else {
-        gameWon = false;
+        gameData.gameWon = false;
     };
     //Collision detection; if the player is within one of the
     //specified ranges of one of the enemies, set the player
@@ -125,7 +122,7 @@ player.prototype.update = function() {
             player.y < allEnemies[i].y + enemyDistance) {
             player.x = playerXStart;
             player.y = playerYStart;
-            gameLives -= 1;
+            gameData.gameLives -= 1;
         };
     };
 };
@@ -160,23 +157,7 @@ var allEnemies = [];
 
 //Create the Enemies object to store globally available
 //data on enemies
-var Enemies = {
-    //level object contains values for number of 
-    //enemies to appear in the game; TODO allow for 
-    ///easy, medium or hard selection
-    "level": {
-        "Easy": 5,
-        "Medium": 10,
-        "Hard": 20
-    },
-    //rows object contains values for y coordinates of 
-    //enemies to start at
-    "rows": {
-        "row1": 50,
-        "row2": 140,
-        "row3": 220
-    }
-};
+var Enemies = {};
 
 //Spanw function adds enemies to allEnemies array in
 //an amount corresponding to the number supplied
@@ -187,20 +168,20 @@ Enemies.spawn = function(num) {
         //divisibility of the index
         switch (i % 3) {
             case 0:
-                allEnemies[i] = new Enemy(enemyXStart, Enemies.rows.row1);
+                allEnemies[i] = new Enemy(enemyXStart, gameData.rows.row1);
                 break;
             case 1:
-                allEnemies[i] = new Enemy(enemyXStart, Enemies.rows.row2);
+                allEnemies[i] = new Enemy(enemyXStart, gameData.rows.row2);
                 break;
             case 2:
-                allEnemies[i] = new Enemy(enemyXStart, Enemies.rows.row3);
+                allEnemies[i] = new Enemy(enemyXStart, gameData.rows.row3);
                 break;
         }; 
     };
 };
 
 //Call the enemies.spawn function to populate the allEnemies array
-Enemies.spawn(gameLevel * 5);
+Enemies.spawn(gameData.gameLevel * 5);
 
 //Place the player object in a variable called player
 var player = new player();
@@ -227,10 +208,10 @@ var headsUpDisplay = {
 };
 
 headsUpDisplay.render = function() {
-    ctx.fillText(headsUpDisplay.Lives + gameLives, 325, 80);
+    ctx.fillText(headsUpDisplay.Lives + gameData.gameLives, 325, 80);
     ctx.font = "bold 20px Quicksand";
     ctx.fillStyle = "yellow";
-    ctx.fillText(headsUpDisplay.Level + gameLevel, 5, 80);
+    ctx.fillText(headsUpDisplay.Level + gameData.gameLevel, 5, 80);
     ctx.font = "bold 20px Quicksand";
     ctx.fillStyle = "yellow";
 };
